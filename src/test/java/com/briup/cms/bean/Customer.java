eebdf2dc6c10c973bcbf2844083b8afe5b1c0381
yuyj <534617908@qq.com>
2019-11-25 10:17:25 +0800

    初始化项目

---
+++ src/test/java/com/briup/cms/bean/Customer.java
@@ -0,0 +1,6 @@
+package com.briup.cms.bean;
+
+import java.io.Serializable;
+
+public class Customer implements Serializable {
+}
---
+++ src/test/java/com/briup/cms/bean/Link.java
@@ -0,0 +1,14 @@
+package com.briup.cms.bean;
+
+import com.sun.javafx.beans.IDProperty;
+
+import javax.annotation.Generated;
+import java.io.Serializable;
+public class Link implements Serializable {
+
+    private int id;
+    private String name;
+    private String url;
+
+
+}
